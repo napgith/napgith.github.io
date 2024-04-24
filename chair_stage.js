d50b4182084b2b0bddd6eedfbb9e42f0cf3fb962
napgith <159562394+napgith@users.noreply.github.com>
2024-04-24 11:56:11 -0500

    Add files via upload

--- chair_stage.js
+++ chair_stage.js
@@ -1,17 +1,17 @@
 // function to progress to next stage 
 var chair3 = function() {
-    alert('This Chair is the right option......good job');
+    alert('The small chair is the right option......good job');
     location.replace("bed_stage.html");
 }
 
 // function to end game 
 var chair1 = function() {
-    alert('Oh Oh......Goldilocks chose Chair 1..... too high');
+    alert('......Goldilocks chose Big chair..... too high');
     location.replace("game_over2.html");
 }
 
 // function to end game  
 var chair2 = function() {
-    alert('......Goldilocks chose Chair 2..... too high');
+    alert('......Goldilocks chose the medium chair..... a bit too high');
     location.replace("game_over2.html");
 }
